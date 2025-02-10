fc33d036084ec9e0df3963c9eee04d9fe4e0ba37
peasent3 <kinjie.supe3@gmail.com>
2025-02-10 12:24:45 -0500

    Update myproject.js

--- myproject.js
+++ myproject.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const hostname = '192.168.1.122.';
+const hostname = '192.168.1.122';
 const port = 8080;
 
 const server = http.createServer((req,res) => {
