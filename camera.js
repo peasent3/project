9813f794638a36391f04be83eb1655812cece485
peasent3 <kinjie.supe3@gmail.com>
2025-02-10 13:58:24 -0500

    Update camera.js

--- camera.js
+++ camera.js
@@ -16,6 +16,25 @@ app.get('/capture', async (req, res) => {
   }
 });
 
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+      // read the html file
+    fs.readFile('project/test.html', (err, data) => {
+    if (err) {
+      // handle error
+      res.writeHead(404);
+      res.write('File not found!');
+    } else {
+      // send the html code as the response
+      res.write(data);
+    }
+
+    // end the response
+    res.end();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
